1686cc3fcdb00bbe7399116226b8a55ccad515d5
mobin <mobin-888@163.com>
2016-12-30 22:21:42 +0800

    dataExtension

--- SwiftExtension/DataExtensions.swift
+++ SwiftExtension/DataExtensions.swift
@@ -11,13 +11,15 @@ import UIKit
 // MARK: - Properties
 public extension Data {
     
-    /// NSAttributedString from Data (if applicable).
+    /// NSAttributedString from HTMLText Data  (if applicable).
     var attributedString: NSAttributedString? {
         // http://stackoverflow.com/questions/39248092/nsattributedstring-extension-in-swift-3
         do {
             return try NSAttributedString(data: self, options: [
                 NSDocumentTypeDocumentAttribute : NSHTMLTextDocumentType,
                 NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue], documentAttributes: nil)
+            
+            
         } catch _ {
             return nil
         }
