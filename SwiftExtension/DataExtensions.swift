a26a258c9507acd2cbc74db44cc840a188b6c729
mobin <mobin-888@163.com>
2016-12-30 22:16:34 +0800

    data

---
+++ SwiftExtension/DataExtensions.swift
@@ -0,0 +1,26 @@
+//
+//  DataExtensions.swift
+//  SwiftExtension
+//
+//  Created by user on 16/12/30.
+//  Copyright © 2016年 mobin. All rights reserved.
+//
+
+import UIKit
+
+// MARK: - Properties
+public extension Data {
+    
+    /// NSAttributedString from Data (if applicable).
+    var attributedString: NSAttributedString? {
+        // http://stackoverflow.com/questions/39248092/nsattributedstring-extension-in-swift-3
+        do {
+            return try NSAttributedString(data: self, options: [
+                NSDocumentTypeDocumentAttribute : NSHTMLTextDocumentType,
+                NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue], documentAttributes: nil)
+        } catch _ {
+            return nil
+        }
+    }
+}
+
